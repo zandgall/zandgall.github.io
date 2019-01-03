c855b6e127f7abacd709bf0d34d03bdaffc4abdb
Zandgall <38407831+zandgall@users.noreply.github.com>
2019-01-02 18:30:59 -0600

    Arvopia 0.7 build 1

--- layers.js
+++ layers.js
@@ -1,6 +1,6 @@
-var o1 = -50, o2 = 50, o3 = 200;
-var r1 = 8, r2 = 4, r3 = 1.75;
-var s1 = Math.random()+1, s2=Math.random()+1, s3=Math.random()+1;
+var o1 = -150, o2 = -50, o3 = 100;
+var r1 = 8, r2 = 4, r3 = 1.75, r4 = 16;
+var s1 = Math.random()+1, s2=Math.random()+1, s3=Math.random()+1, s4=Math.random();
 
 function display() {
     var can = document.getElementById("BackgroundC");
@@ -15,7 +15,11 @@ function display() {
     var img0 = document.getElementById("BGImg0");
     var img1 = document.getElementById("BGImg1");
     var img2 = document.getElementById("BGImg2");
-            
+    
+    var img3 = new Image();
+    img3.src = "assets/Sun.png";
+    
+    c.drawImage(img3, can.width*s4, 150-scrollY/r4);
     c.drawImage(img0, can.width-3240*s1, o1-scrollY/r1, 6480, 2322);
     c.drawImage(img1, can.width-3440*s2, o2-scrollY/r2, 6880, 2722);
     c.drawImage(img2, can.width-3640*s3, o3-scrollY/r3, 7280, 3122);
